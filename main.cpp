b61243b3fe5b3a8c5517b3f2108ecdaea25f1b73
ZaitsevaPOLINA <ZPoolly@gmail.com>
2016-12-14 13:19:34 +0300

    gghju

---
+++ main.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+#include <string>
+int f( double x, int n, int a[n]){
+if (a[n/2]==x) cout « "yes";
+else if (a[n/2]>x) return int new a[n]=a[n/2];
+else if (a[n/2]>x) for ( p=0; p<=n/2; p++) a[p]=a[p+n/2]return int new a[n]=a[p];
+}
+int main()
+{
+int n;
+double x;
+int a[n];
+cin » n;
+for (int i=0; i<=n; i++);
+cin » a[i];
+cin » x;
+cout « f( double x, int a[n]);
+return 0;
+}
